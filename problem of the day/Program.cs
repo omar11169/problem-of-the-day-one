2c2fae668abbc8557fe390352c2c194be613e17c
omar11169 <omar-albalushi@outlook.com>
2025-05-12 10:41:09 +0400

    Problem of the day one - Push

---
+++ problem of the day/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace problem_of_the_day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int black = 1;
+            int white = 1;
+            int height = 1;
+
+            for (int i = 1; i < 100; i++)
+            {
+                if ((i % 2) == 0)
+
+                    if (black >= i)
+                    {
+                        black = black - i;
+                        height++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                
+            }
+                white--;
+                height++;
+            Console.WriteLine($"{height}");
+        }
+    }
+}
